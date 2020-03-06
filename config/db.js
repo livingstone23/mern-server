292f96a33e98806f2ff5715870ad4e9dbd89cd24
Livingstone <livingstone23@gmail.com>
2020-03-06 10:24:20 -0600

    ajuste para deployment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path: 'variables.env'});
+const dbRoute = 'mongodb+srv://sa:ABCabc123@cluster0-apldc.mongodb.net/merntasks';
 
 const conectarDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        });
+    try { 
+                await 
+                //mongoose.connect(process.env.DB_MONGO, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+                mongoose.connect(dbRoute, { useNewUrlParser: true }).then(() => { console.log('Connected to mongoDB')})
+
 
         console.log('DB Conectada');
 
