8208585208c552ce2bb4da6c86aed0437c103a9e
Livingstone <livingstone23@gmail.com>
2020-03-06 10:55:00 -0600

    ajuste en mensaje deployment

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const dbRoute = 'mongodb+srv://sa:ABCabc123@cluster0-apldc.mongodb.net/merntasks
 const conectarDB = async () => {
     try { 
                 await 
-                //mongoose.connect(process.env.DB_MONGO, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+                mongoose.connect(process.env.DB_MONGO, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
-                mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, reconnectTries: 30, reconnectInterval: 500 }).then(() => { console.log('Connected to mongoDB')})
+                //mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, reconnectTries: 30, reconnectInterval: 500 }).then(() => { console.log('Connected to mongoDB')})
 
 
         console.log('DB Conectada');
